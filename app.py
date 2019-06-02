1176e7cb42743d2a8f6abdfc3bda5c3a26b761e6
Hunter Cash <hmclaug@gmail.com>
2019-06-02 17:02:22 -0500

    Heroku Set up

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask, jsonify, render_template
 import flask_sqlalchemy
 import config
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/data")
+    return jsonify({"data":"is empty"})
+
+if __name__ == "__main__":
+    app.run()
